f8aa96019372d2e0f01cfa1e522febdf1d2ddece
Marco Nembrini <marco@3ap.ch>
2019-03-13 17:32:24 +0100

    add grid service interface only

---
+++ gol/src/main/java/ch/mnembrini/kata/gol/GenerationService.java
@@ -0,0 +1,16 @@
+package ch.mnembrini.kata.gol;
+
+
+public class GenerationService {
+
+
+    /**
+     * Advance the game by one generation
+     *
+     * @param grid the game state
+     */
+    public void next(Grid grid) {
+
+    }
+}
+
