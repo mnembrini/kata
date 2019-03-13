d7cbdc39b4cc4fb32a148160be55c852aaf1ca69
Marco Nembrini <marco@3ap.ch>
2019-03-13 20:48:24 +0100

    add next state rules

--- gol/src/main/java/ch/mnembrini/kata/gol/GenerationService.java
+++ gol/src/main/java/ch/mnembrini/kata/gol/GenerationService.java
@@ -9,22 +9,48 @@ public class GenerationService {
      *
      * @param grid the game state
      */
-    public void next(Grid grid) {
+    public void advance(Grid grid) {
 
         for (int row = 0; row < grid.getRowSize(); row++) {
             for (int column = 0; column < grid.getColumnSize(); column++) {
-                boolean alive = grid.getCell(row, column);
-                int aliveNeighbours = grid.countAliveNeighbours(row, column);
-                boolean nextState = computeNextState(alive, aliveNeighbours);
-                grid.setCell(row, column, nextState);
+                advanceCell(grid, row, column);
             }
         }
 
 
     }
 
+    private void advanceCell(Grid grid, int row, int column) {
+        boolean alive = grid.getCell(row, column);
+        int aliveNeighbours = grid.countAliveNeighbours(row, column);
+        boolean nextState = computeNextState(alive, aliveNeighbours);
+        grid.setCell(row, column, nextState);
+    }
+
     private boolean computeNextState(boolean alive, int aliveNeighbours) {
-        return false;
+        if (alive) {
+            // rule 1
+            // under population
+            if (aliveNeighbours < 2) {
+                return false;
+            }
+            // rule 2
+            // over population
+            else if (aliveNeighbours > 3) {
+                return false;
+            }
+            // rule 3
+            else {
+                return true;
+            }
+        } else {
+            // rule 4
+            if (aliveNeighbours == 3) {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
 
